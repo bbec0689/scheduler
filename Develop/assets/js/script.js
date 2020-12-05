7c865d1763c03f0fdcf69f1ab2ad36abd7bbdbb3
Dave <bbec0689@gmail.com>
2020-12-05 15:56:12 -0700

    formatted folders more and added time columns

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,14 @@
+// // Code for the day and date
+// var todayDate = moment().format('dddd, MMM Do YYYY');
+// $("#currentDay").html(todayDate);
+
+// $(document).ready(function () {
+//     // saveBtn click listener 
+//     $(".saveBtn").on("click", function () {
+//         // Get nearby values of the description in JQuery
+//         var text = $(this).siblings(".description").val();
+//         var time = $(this).parent().attr("id");
+
+//         // Save text in local storage
+//         localStorage.setItem(time, text);
+//     }
