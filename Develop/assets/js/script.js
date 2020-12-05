8833492e5f7881969be21fefca2a1df15d67c3e1
Dave <bbec0689@gmail.com>
2020-12-05 16:49:09 -0700

    finished JS updated css colors for the background and updated html for code that was not working

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -5,7 +5,8 @@ $("#currentDay").html(todayDate);
 $(document).ready(function () {
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
+
+        // Get values of the description
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
@@ -14,12 +15,45 @@ $(document).ready(function () {
     });
 });
 
-    function timeKeeper() {
+    function timeTracker() {
         //get hours
         var timeNow = moment().hour();
 
         //creating loop for time blocks
         $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1])
+            var blockTime = parseInt($(this).attr("id").split("time")[1]);
+
+            //checking time and adding the classes
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
         })
     }
+
+        // getItem from local storage
+        $("#time8 .description").val(localStorage.getItem("time8"));
+        $("#time9 .description").val(localStorage.getItem("time9"));
+        $("#time10 .description").val(localStorage.getItem("time10"));
+        $("#time11 .description").val(localStorage.getItem("time11"));
+        $("#time12 .description").val(localStorage.getItem("time12"));
+        $("#time13 .description").val(localStorage.getItem("time13"));
+        $("#time14 .description").val(localStorage.getItem("time14"));
+        $("#time15 .description").val(localStorage.getItem("time15"));
+        $("#time16 .description").val(localStorage.getItem("time16"));
+        $("#time17 .description").val(localStorage.getItem("time17"));
+        //returning time tracker function
+        timeTracker();
+    
