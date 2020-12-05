a8004fa6b64a2f70f6b7d15a9105008f338a5097
Dave <bbec0689@gmail.com>
2020-12-05 16:07:47 -0700

    added some js and added @media to css for phones and tables

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,14 +1,25 @@
-// // Code for the day and date
-// var todayDate = moment().format('dddd, MMM Do YYYY');
-// $("#currentDay").html(todayDate);
+// Code for the day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-// $(document).ready(function () {
-//     // saveBtn click listener 
-//     $(".saveBtn").on("click", function () {
-//         // Get nearby values of the description in JQuery
-//         var text = $(this).siblings(".description").val();
-//         var time = $(this).parent().attr("id");
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-//         // Save text in local storage
-//         localStorage.setItem(time, text);
-//     }
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    });
+});
+
+    function timeKeeper() {
+        //get hours
+        var timeNow = moment().hour();
+
+        //creating loop for time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1])
+        })
+    }
